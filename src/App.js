8b765c9c03360dd90dc0164c5dbdee44c2457f48
dev ads mania <dev@mania.tn>
2020-11-24 20:27:08 +0100

    Styling the Component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ class App extends React.Component {
 		return (
 			<div>
         <Search/>
-        this is search component
 			</div>
 		);
 	}
--- src/components/SearchComponent/Search.js
+++ src/components/SearchComponent/Search.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import './Search.css';
 
 class Search extends  React.Component {
-
+// Initilise some information in state
+    constructor( props ) {
+		super( props );
+		this.state = {
+			query: '',
+                        results: {},
+                        loading: false,
+                        message: '',
+		};
+    }
+    
 render() {
     return (
-        <div>
-        
+        <div className="container">
+            {/*Heading*/}
+            <h2 className="heading">Github User Search</h2>
+            {/*Search Input*/}
+            <label className="search-label" htmlFor="search-input">
+                <input
+                    type="text"
+                    value=""
+                    id="search-input"
+                    placeholder="Search..."
+                />
+                <i className="fa fa-search search-icon" aria-hidden="true"/>
+            </label>
+            
         </div>
-    )
+        )
 }
 
 }
