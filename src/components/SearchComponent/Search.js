5649389f6dfdbaac83e8889fea16e3d6f36351f6
dev ads mania <dev@mania.tn>
2020-11-24 21:40:10 +0100

    Loading Data From API

--- src/components/SearchComponent/Search.js
+++ src/components/SearchComponent/Search.js
@@ -7,17 +7,59 @@ class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      query: "",
-      results: {},
-      loading: false,
-      message: "",
+        query: '',
+        results: {},
+        loading: false,
+        message: '',
+        totalResults: 0,
     };
+    this.cancel = '';
   }
-  // Handle input value
-  handleOnInputChange = (event) => {
+// fetching for users
+fetchSearchResults = ( query ) => {
+    const searchUrl = ` https://api.github.com/search/users?q=${query}`;
+
+    if( this.cancel ) {
+        this.cancel.cancel();
+    }
+
+    this.cancel = axios.CancelToken.source();
+
+    axios.get( searchUrl, {
+        cancelToken: this.cancel.token
+    } )
+        .then(  res => {
+            console.warn(res);
+            } )
+        
+        .catch( error => {
+            if ( axios.isCancel(error) || error ) {
+                this.setState({
+                    loading: false,
+                    message: 'Failed to fetch the data. Please check network'
+                })
+            }
+        } )
+};
+
+handleOnInputChange = ( event ) => {
     const query = event.target.value;
-    this.setState({ query:query, loading: true, message: "" });
-  };
+    if ( ! query ) {
+        this.setState( { query, results: {}, message: '' } );
+    } else {
+        this.setState( { query, loading: true, message: '' }, () => {
+            this.fetchSearchResults( query );
+        } );
+    }
+};
+
+/**
+ * Fetch results according to the prev or next page requests.
+ *
+ * @param {String} type 'prev' or 'next'
+ */
+
+
 
   render() {
     const { query } = this.state;
